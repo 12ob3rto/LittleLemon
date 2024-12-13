d201653185f411750772ccfa7ab238e93dcdf841
Roberto Alvarez <120bertoag@gmail.com>
2024-12-13 03:58:15 -0600

    Modelos creados

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BookingTable(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    numberGuest = models.IntegerField()
+    bookingDate = models.DateTimeField(db_index=True)
+
+class MenuTable(models.Model):
+    id = models.IntegerField(primary_key=True)
+    tittle = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+
